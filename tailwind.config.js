c044ff1e4a1c62f145c61fe602621e515c903a87
Abneet K Pabyal <Abneet.P@gmail.com>
2023-03-01 11:24:04 -0700

    Layout / setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,52 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xsm: "480px",
+      sm: "610px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    fontFamily: {
+      heading: ["Century Gothic", "sans-serif"],
+      title: ["Montserrat", "sans-serif"],
+      body: ["Roboto", "sans-serif"],
+    },
   },
   plugins: [],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#14186F",
+          secondary: "#D7084E",
+          accent: "#D3B037",
+          neutral: "#FF6E00",
+          "base-100": "#ffff",
+          info: "#ffff",
+          success: "#16a34a",
+          warning: "#F8DA63",
+          error: "#E23D32",
+        },
+      },
+      {
+        dark: {
+          primary: "#14186F",
+          secondary: "#D7084E",
+          accent: "#D3B037",
+          neutral: "#FF6E00",
+          "base-100": "#192e5b",
+          info: "#313131",
+          success: "#16a34a",
+          warning: "#F8DA63",
+          error: "#E23D32",
+        },
+      },
+    ],
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+  },
 };
