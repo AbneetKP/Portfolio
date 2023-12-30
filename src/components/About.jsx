b980deeb3ed1db4f54645be8d5dca05f7bdb6e0b
Abneet K Pabyal <Abneet.P@gmail.com>
2023-12-29 23:28:17 -0700

    Spelling and Grammar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,18 +11,13 @@ function About() {
                 <p className="py-2"> 
                 I am first generation Canadian, born in Surrey British Columbia and moved to
                 <span className="text-[#d4af37]"> Calgary</span> in 2012. I have background in Marketing, and recently
-                completed a <span className="text-[#d4af37]"> fullstack developer program </span>. I am adept
-                <span className="text-[#d4af37]"> JavaScript, HTML, CSS </span> and steadily learning more surronding the
-                industry and abundance of open resources. I enjoy
-                <span className="text-[#d4af37]"> digital marketing </span> and <span className="text-[#d4af37]">web development </span>as these are
-                my core passions, and the concept of always being introduced new methods,
-                frameworks, and efficencies of the industries really drew me in.
+                completed a <span className="text-[#d4af37]"> fullstack developer program </span>. I am adept in
+                <span className="text-[#d4af37]"> JavaScript, HTML, CSS </span>  steadily learning more about the industry and the abundance of open resources. 
+                I enjoy <span className="text-[#d4af37]"> digital marketing </span> and <span className="text-[#d4af37]">web development </span>as these are
+                my core passionsthese are my core passions, and the concept of always being introduced to new methods, frameworks, and efficiencies in the industries really drew me in.
                 </p>
-                <p className='py-2'>
-                     2022 of August, I had no prior knowledge of anything related to web or software devleopment.
-                     I was enticed by the industry after visiting San Fransico, it had peaked my interest, and 
-                     I took a deep dive soon after into another field after completing my first degree. 
-                     I believe my desire for Front-End deveolpment is nicely complimented by my marketing knowledge.
+                <p className='py-2'>  
+                In August 2022, I had no prior knowledge of anything related to web or software development. I was enticed by the industry after visiting San Francisco; it had piqued my interest, and I took a deep dive soon after into another field after completing my first degree. I believe my desire for Front-End development is nicely complemented by my marketing knowledge.
                 </p>
                
             </div>
