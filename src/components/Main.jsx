296cdbd2426bc52b72e5db3acd43d9e2b05e819c
Abneet K Pabyal <Abneet.P@gmail.com>
2024-01-01 19:08:36 -0700

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,8 +9,8 @@ function Main() {
         <div className="uppercase text-sm tracking-widest text-gray-600">
             <p>Oh hey there, you found me, Lets build together</p>
             <h1 className="py-4 text-gray-700"> I am <span className='text-[#d4af37]'> Abneet</span></h1>
-            <h1 className="py-2 text-gray-600"> A Front-End Web Developer </h1>
-            <p className="py-4 text-gray-600 max-w-[70%] m-auto"> I am a frontend developer who specializes in building seamless digital user experiences. I recenlty completed a certification for Fullstack and found myself immersed in Front-End web developement. In the last 7 months my skills have quickly flourished.</p>
+            <h1 className="py-2 text-gray-600"> Marketing / Home Development </h1>
+            <p className="py-4 text-gray-600 max-w-[70%] m-auto"> I am currently a project manager for Pabyal Construction and actively seeking new opportunties to learn and grow in my career.</p>
             <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                         <a href="https://www.linkedin.com/in/abneetpabyal/">
                            <div className=" rounded-full shadow-xl shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300" >
